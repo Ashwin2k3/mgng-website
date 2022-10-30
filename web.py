574ebb0e0c68e4d95aa82bfb6fd697e68076da26
Ashwin2k3 <ashwin.2k3@gmail.com>
2022-10-30 12:00:56 +0530

    added html and layout

--- web.py
+++ web.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 web = Flask(__name__)
 
 @web.route("/")
 def website():
-    return "mgng"
+    return render_template("home.html")
 
 if __name__ =="__main__":
     web.run(debug = True)
